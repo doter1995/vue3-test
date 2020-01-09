0ee8bdad357bd71a00cf5444d18c157a32c1498f
weiDong ZHang <doter1995@qq.com>
2020-01-09 20:46:12 +0800

    remove log

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import {createApp } from "vue";
 
 import App from "./App.vue";
-window.app = App
-console.log(App)
 let root = document.createElement("div");
 document.body.append(root);
 createApp().mount(App,root)
